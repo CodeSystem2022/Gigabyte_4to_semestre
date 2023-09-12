977b11aab3239a43b580cdc0b7c65627a123041c
Ramiro Valentin Riquelme <89102749+RamiroRiquelme@users.noreply.github.com>
2023-09-11 21:50:28 -0300

    Añado clase 2 de e-commerce

--- Javascript/e-commerce2022/client/js/products.js
+++ Javascript/e-commerce2022/client/js/products.js
@@ -3,27 +3,27 @@ const productos = [{
         productName: "Banana",
         price: 480,
         quanty: 1,
-        img: "/client/media/banana.png"
+        img: "/e-commerce2022/client/media/banana.png"
     },
     {
         id: 2,
         productName: "Leche",
         price: 950,
         quanty: 1,
-        img: "/client/media/leche.png"
+        img: "/e-commerce2022/client/media/leche.png"
     },
     {
         id: 3,
         productName: "Pollo",
         price: 750,
         quanty: 1,
-        img: "/client/media/pollo.png"
+        img: "/e-commerce2022/client/media/pollo.png"
     },
     {
         id: 4,
         productName: "Mayonesa",
         price: 480,
         quanty: 1,
-        img: "/client/media/mayonesa.png"
+        img: "/e-commerce2022/client/media/mayonesa.png"
     }
 ]
